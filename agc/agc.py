b1f849fa6411c756e2c9d0dc8d39415c24f49f91
Paul Uteza <utezapaul@eisti.eu>
2021-01-10 14:55:52 +0100

    Step 2 + pylint

--- agc/agc.py
+++ agc/agc.py
@@ -18,18 +18,18 @@ import sys
 import os
 import gzip
 import statistics
-from collections import Counter, OrderedDict
+from collections import Counter
 # https://github.com/briney/nwalign3
 # ftp://ftp.ncbi.nih.gov/blast/matrices/
 import nwalign3 as nw
 
-__author__ = "Your Name"
-__copyright__ = "Universite Paris Diderot"
-__credits__ = ["Your Name"]
+__author__ = "UTEZA Paul"
+__copyright__ = "CY Tech"
+__credits__ = ["UTEZA Paul"]
 __license__ = "GPL"
 __version__ = "1.0.0"
-__maintainer__ = "Your Name"
-__email__ = "your@email.fr"
+__maintainer__ = "UTEZA Paul"
+__email__ = "utezapaul@eisti.eu"
 __status__ = "Developpement"
 
 
@@ -55,7 +55,7 @@ def get_arguments():
     parser = argparse.ArgumentParser(description=__doc__, usage=
                                      "{0} -h"
                                      .format(sys.argv[0]))
-    parser.add_argument('-i', '-amplicon_file', dest='amplicon_file', type=isfile, required=True, 
+    parser.add_argument('-i', '-amplicon_file', dest='amplicon_file', type=isfile, required=True,
                         help="Amplicon is a compressed fasta file (.fasta.gz)")
     parser.add_argument('-s', '-minseqlen', dest='minseqlen', type=int, default = 400,
                         help="Minimum sequence length for dereplication")
@@ -71,10 +71,9 @@ def get_arguments():
 
 def read_fasta(amplicon_file, minseqlen):
     """
-
+    Read amplicon file and generate FASTA sequences superior of a certain length
     :param amplicon_file: File with FASTA sequences
-    :param minseqlen:
-    :return:
+    :param minseqlen: minimum length of sequences we will consider
     """
     with gzip.open(amplicon_file, "rt") as file:
         sequence = ""
@@ -93,50 +92,198 @@ def read_fasta(amplicon_file, minseqlen):
         if len(sequence) >= minseqlen:
             yield sequence
 
-
-
-
-
 def dereplication_fulllength(amplicon_file, minseqlen, mincount):
+    """
+    Generate unique sequences and corresponding occurence count with more occurences than mincount
+    :param amplicon_file: File containing FASTA sequences
+    :param minseqlen: minimum length of sequences we will consider
+    :param mincount: minimum occurrence count of sequences we will consider
+    """
     sequences = read_fasta(amplicon_file, minseqlen)
-    sequence_list = [x for x in sequences]
+    sequence_list = list(sequences)
     seq_counter = Counter(sequence_list)
-    print(seq_counter)
-    for seq, count in OrderedDict(sorted(seq_counter.items(), key=lambda t: t[0])).items():
-        print(seq)
-        if count >= mincount:
-            yield [seq, count]
-
+    seq_order = sorted(seq_counter, key=seq_counter.get, reverse=True)
+    for seq in seq_order:
+        if seq_counter[seq] >= mincount:
+            yield [seq, seq_counter[seq]]
 
 
 def get_chunks(sequence, chunk_size):
-    pass
+    """
+    Create list of sub sequences of given size with a minimum of 4 sequences
+    :param sequence: Sequence to split
+    :param chunk_size: Size of sub sequences
+    :return: Sub sequences of given size
+    """
+    if len(sequence)//chunk_size < 4:
+        print('Not enough chunks')
+        raise ValueError
+    return [sequence[i:i + chunk_size] for i in range(0, len(sequence), chunk_size)
+            if len(sequence[i:i + chunk_size]) >= chunk_size]
+
 
 def get_unique(ids):
+    """
+    Return unique keys
+    :param ids: List
+    :return: unique keys
+    """
     return {}.fromkeys(ids).keys()
 
-
-def common(lst1, lst2): 
+def common(lst1, lst2):
+    """
+    Return list of common elements from two lists
+    :param lst1: First list to compare
+    :param lst2: Second list to compare
+    :return: List of unique common elements from both lists
+    """
     return list(set(lst1) & set(lst2))
 
 def cut_kmer(sequence, kmer_size):
-    pass
+    """
+    Take a sequence and generate kmer cut in given size
+    :param read: sequence
+    :param kmer_size: size the kmer needs to be cut
+    """
+    for i in range(len(sequence) - kmer_size + 1):
+        yield sequence[i:kmer_size + i]
 
 def get_identity(alignment_list):
-    pass
+    """
+    Return percentage of identity (similarity) from a list of 2 alignements
+    :param alignment_list: List of alignements
+    :return: Percentage of identity
+    """
+    count = 0
+    for i in range(0, len(alignment_list[0])):
+        if alignment_list[0][i] == alignment_list[1][i]:
+            count +=1
+    return round(count/len(alignment_list[0])*100, 2)
+
+def detect_chimera(perc_indentity_matrix):
+    """
+    Detect a chimera based on rules from an identity matrix
+    :param perc_indentity_matrix: Identity matrix in percentage
+    :return: Boolean depending if it's a chimera or not
+    """
+    seq_1 = []
+    seq_2 = []
+    std_perc = 0
+    for perc_id in perc_indentity_matrix:
+        std_perc += statistics.stdev(perc_id)
+        seq_1.append(perc_id[0])
+        seq_2.append(perc_id[1])
+
+    if len(set(seq_1)) >= 2 or len(set(seq_2)) >= 2:
+        if std_perc/len(perc_id) > 5.0:
+            return True
+    return False
+
+
+def get_unique_kmer(kmer_dict, sequence, id_seq, kmer_size):
+    """
+    Create dictionary of kmer with their sequence id as value
+    :param kmer_dict: dictionary of kmer
+    :param sequence: sequence to consider
+    :param id_seq: id of the sequence
+    :param kmer_size: size the kmer needs to be cut
+    :return: Dictionary of kmer with their sequence id as value
+    """
+    cut_kmers = cut_kmer(sequence, kmer_size)
+    for kmer in cut_kmers:
+        if kmer in kmer_dict.keys():
+            kmer_dict[kmer].append(id_seq)
+        else:
+            kmer_dict[kmer] = [id_seq]
+    return kmer_dict
+
+
+def search_mates(kmer_dict, sequence, kmer_size):
+    """
+    Find 8 most similar sequences of given sequence
+    :param kmer_dict: dictionary of kmer
+    :param sequence: sequence to consider
+    :param kmer_size: size the kmer needs to be cut
+    :return: 8 most similar sequences of given sequence
+    """
+    cut_kmers = cut_kmer(sequence, kmer_size)
+    mates = []
+    for kmer in cut_kmers:
+        if kmer in kmer_dict.keys():
+            mates.append(kmer_dict[kmer])
+        else:
+            continue
+    mates = [item for sublist in mates for item in sublist]
+    counter_mates = Counter(mates)
+    best_mates_counter = counter_mates.most_common(8)
+    best_mates = [x[0] for x in best_mates_counter]
+    return best_mates
+
 
 def chimera_removal(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
-    pass
+    """
+    Generate non chimera sequences
+    :param amplicon_file: File containing FASTA sequences
+    :param minseqlen: minimum length of sequences we will consider
+    :param mincount: minimum occurence count of sequences we will consider
+    :param chunk_size: Size of sub sequences
+    :param kmer_size: size the kmer needs to be cut
+    """
+    dereplications = dereplication_fulllength(amplicon_file, minseqlen, mincount)
+    kmer_dict = {}
+    no_chimera = []
+    for seq_id, (dereplication, dereplication_count) in enumerate(dereplications):
+        chunks = get_chunks(dereplication, chunk_size)
+        mates = [search_mates(kmer_dict, chunk, kmer_size) for chunk in chunks]
+        parents = mates[0]
+        for mate in mates:
+            parents = common(parents, mate)[:2]
+        perc_identity_matrix = []
+        if len(parents) == 2:
+            perc_identity_matrix = [[] for _ in chunks]
+            ref_chunks_1 = get_chunks(no_chimera[parents[0]], chunk_size)
+            ref_chunks_2 = get_chunks(no_chimera[parents[1]], chunk_size)
+            for idx, ref in enumerate(ref_chunks_1):
+                perc_identity_matrix[idx] = get_identity(nw.global_align(chunks[idx], ref))
+            for idx, ref in enumerate(ref_chunks_2):
+                perc_identity_matrix[idx] = get_identity(nw.global_align(chunks[idx], ref))
+        if not detect_chimera(perc_identity_matrix):
+            no_chimera.append(dereplication)
+            kmer_dict = get_unique_kmer(kmer_dict, dereplication, seq_id, kmer_size)
+            yield [dereplication, dereplication_count]
+
 
 def abundance_greedy_clustering(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
+    """
+    Greedy clustering using non chimera sequence generator to find
+    if another sequence is similar at 97%+ and more common
+    :param amplicon_file: File containing FASTA sequences
+    :param minseqlen: minimum length of sequences we will consider
+    :param mincount: minimum occurence count of sequences we will consider
+    :param chunk_size: Size of sub sequences
+    :param kmer_size: size the kmer needs to be cut
+    :return: List of OTU
+    """
     pass
 
 def fill(text, width=80):
-    """Split text with a line return to respect fasta format"""
+    """
+    Create correct FASTA format string
+    :param text: Text to split in FASTA format
+    :param width: max width per line
+    :return: String in correct format
+    """
     return os.linesep.join(text[i:i+width] for i in range(0, len(text), width))
 
 def write_OTU(OTU_list, output_file):
+    """
+    Save output in file with FASTA format
+    :param OTU_list: List of OTU sequences
+    :param output_file: file to save
+    """
     pass
+
+
 #==============================================================
 # Main program
 #==============================================================
@@ -144,8 +291,17 @@ def main():
     """
     Main program function
     """
-    # Get arguments
+    # Get args
     args = get_arguments()
+
+    # Get file and variables
+    file = args.amplicon_file
+    minseqlen = args.minseqlen
+    mincount = args.mincount
+    chunk_size = args.chunk_size
+    kmer_size = args.kmer_size
+    output_file = args.output_file
+
 
 
 if __name__ == '__main__':
